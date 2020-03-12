5be9836476b144a736b5b13f19a100d4e424d601
Vicso <vicsographik@gmail.com>
2020-03-11 18:49:47 -0600

    resuelto parcialmente reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import '../styles/styles.css'
-
 import MenuMovil from './modules/MenuMovil'
+import RevealOnScroll from './modules/RevealOnScroll'
+
+let revealOnScroll = new RevealOnScroll();
 
 let menuMovil = new MenuMovil();
 
